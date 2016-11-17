4220389b1d20f559cf5910e0e3ca039cec591ce4
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-16 22:53:14 -0500

    fixed project data in helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -11,26 +11,26 @@ for(var i = 0; i < skillsArray.length; i++) {
 }
 
 // Projects
-var projects = '<li><h3>%data%</h3><div class="row"><div class="col-md-3"><img src="./img/%data1%.gif" alt="project-images" class="img-responsive"></div><div class="col-md-9"><h3>Languages(s): %data2% </h3><h5>%data3%</h5></div></div></li><br><hr>';
+var projects = '<li><h3>%data%</h3><div class="row"><div class="col-md-3"><img src="./img/%data1%.gif" alt="project-images" class="img-responsive"></div><div class="col-md-9"><h3>Languages: %data2% </h3><h5>%data3%</h5></div></div></li><br><hr>';
 
 var projectsArray = [
   {
     name: 'Connect Four',
     img: 'a',
-    language: 'HTML/CSS Javascript, and jQuery' ,
+    language: 'HTML/CSS, JavaScript, and jQuery' ,
     info: 'A desktop application version of the original two player game. The object is for two players to compete by connecting four chips either vertically, horizontally or diagonally first. This app was designed using HTML. JavaScript was used for mathematical calculations in order to find the accurate winner and jQuery was used to manipulate DOM elements.'
   },
   {
     name: 'Movie Collection' ,
     img: 'a',
-    language: 'Python',
-    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    language: 'HTML/CSS, JavaScript, MongoDB, Mongoose, and Express Node.js ',
+    info: 'An application that enables users to share and edit their own movies. Users can sign up, login, logout, and even change their account information. This application has a working database and is hosted on Heroku. Technologies used are HTML, CSS, JavaScript, MongoDB, Mongoose, and Express Node.js'
   },
   {
     name: 'The Store Front',
     img: 'a',
-    language: 'PHP',
-    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    language: 'HTML/CSS, JavaScript, MongoDB, Mongoose, and Angular',
+    info: "Full stack application that utilizes full user authentication. Store Front is a team built, multi-product store application with the ability to add, edit and delete products from a cart and view user's shopping history. This application is deployed on Heroku. Technologies used are HTML, CSS, JavaScript, MongoDB, Mongoose, and Angular"
   }
 ];
 
