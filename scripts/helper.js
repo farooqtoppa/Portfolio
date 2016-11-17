f56542be92a6da38bc123e3f133f9e6e8b2ac585
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-16 22:19:02 -0500

    added about me

--- scripts/helper.js
+++ scripts/helper.js
@@ -11,14 +11,14 @@ for(var i = 0; i < skillsArray.length; i++) {
 }
 
 // Projects
-var projects = '<li><h3>%data%</h3><div class="row"><div class="col-md-3"><img src="./img/%data1%.gif" alt="project-images" class="img-responsive"></div><div class="col-md-9"><h3>Languages(s): %data2% </h3><h5>%data3%</h5></div></div></li>'
+var projects = '<li><h3>%data%</h3><div class="row"><div class="col-md-3"><img src="./img/%data1%.gif" alt="project-images" class="img-responsive"></div><div class="col-md-9"><h3>Languages(s): %data2% </h3><h5>%data3%</h5></div></div></li><br><hr>';
 
 var projectsArray = [
   {
     name: 'Connect Four',
     img: 'a',
-    language: 'Swift' ,
-    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    language: 'HTML/CSS Javascript, and jQuery' ,
+    info: 'A desktop application version of the original two player game. The object is for two players to compete by connecting four chips either vertically, horizontally or diagonally first. This app was designed using HTML. JavaScript was used for mathematical calculations in order to find the accurate winner and jQuery was used to manipulate DOM elements.'
   },
   {
     name: 'Movie Collection' ,
